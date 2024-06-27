b47640cffd448eb84a1cf46d5d79b5e6d0e8f997
phat204 <phatdtph37313@fpt.edu.vn>
2024-06-27 20:12:05 +0700

    lab1

---
+++ app/src/main/java/demo_1/Adapter.java
@@ -0,0 +1,40 @@
+package demo_1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import ph37313.poly.bai1.R;
+
+public class Adapter extends ArrayAdapter<Model> {
+    public Adapter(@NonNull Context context, List<Model> models) {
+        super(context, 0, models);
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_view, parent, false);
+        }
+        Model model = getItem(position);
+        if (model != null) {
+            TextView tvTit = convertView.findViewById(R.id.tvTit);
+            TextView tvDes = convertView.findViewById(R.id.tvDes);
+
+            tvTit.setText(model.getTitle());
+            tvDes.setText(model.getDescription());
+//            tvTit.setText("Title: " + model.getTitle());
+//            tvDes.setText("Description: " + model.getDescription());
+        }
+        return convertView;
+    }
+}
---
+++ app/src/main/java/demo_1/demo_1.java
@@ -0,0 +1,58 @@
+package demo_1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+import ph37313.poly.bai1.R;
+
+public class demo_1 extends AppCompatActivity {
+
+    private Adapter adapter;
+    private List<Model> list;
+    dbheper dbhepers;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_demo1);
+
+        ListView listView = findViewById(R.id.lvDemo1);
+        TextView edTit = findViewById(R.id.edTit);
+        TextView edDes = findViewById(R.id.edDes);
+        Button btnThem = findViewById(R.id.btnSub);
+        dbhepers = new dbheper(this);
+        list = dbhepers.getAllData();
+        adapter = new Adapter(this, list);
+        listView.setAdapter(adapter);
+
+        btnThem.setOnClickListener(v -> {
+            String tit = edTit.getText().toString();
+            String des = edDes.getText().toString();
+            if (!tit.isEmpty() && !des.isEmpty()) {
+                Model model = new Model(tit, des);
+                dbhepers.them(model);
+                list.add(model);
+                adapter.notifyDataSetChanged();
+                edTit.setText("");
+                edDes.setText("");
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
